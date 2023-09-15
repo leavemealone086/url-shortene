4459c6393dd78af01ed1f959577d1776df38a2c7
Pony <88284661+leavemealone086@users.noreply.github.com>
2023-09-16 01:24:47 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import {Route, BrowserRouter as Router,Routes } from 'react-router-dom'
-import Input  from './Input'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Redirect from './Redirect'
-import {QRCodeSVG} from 'qrcode.react';
-import ParticlesBg from 'particles-bg'
+import React, { useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Input from './Input';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Redirect from './Redirect';
+import { QRCodeSVG } from 'qrcode.react';
+import ParticlesBg from 'particles-bg';
 
 function App() {
-  return (
+  const [slug, setSlug] = useState('');
 
+  return (
     <div>
       <>
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Input />} />
-          <Route path="/:slug" element={<Redirect />} />
-        </Routes>
-      </Router>
-      <p className='container'> QR code</p>
+        <div>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Input setSlug={setSlug} />} />
+              <Route path="/:slug" element={<Redirect />} />
+            </Routes>
+          </Router>
+          <p className='container'> QR code</p>
 
-      <QRCodeSVG value={'/:slug'} size="256"/>
+          <QRCodeSVG value={slug} size="256" />
+          
+        </div>
 
-      
+        <ParticlesBg type="cobweb" bg={true} />
+      </>
     </div>
-
-    <ParticlesBg type="cobweb" bg={true} />
-    </>
-
-   </div>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
